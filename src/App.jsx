96b29d54b46cba71b549887756dbf11150e0a594
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-09-04 16:30:11 -0300

    vou mudar depois, mas salva o que ja ta pronto aqui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import About from "./components/About"
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
+      <About />
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,42 @@
+import '../styles/About.css'
+
+const About = () => {
+    return (
+        <section className="about-container">
+            <section className="section section1">
+                <div className="section-txt">
+                    <h2>Lorem ipsum dolor sit amet </h2>
+                    <p>Nemo, sequi sit? Qui ipsa rerum impedit explicabo modi, dolore fugiat quod deleniti provident perspiciatis vel at quia doloremque, nesciunt temporibus laudantium!</p>
+                </div>
+                
+                <div className="section-img">
+                    <img src="" alt="imagem" />
+                </div>
+            </section>
+
+            <section className="section section2 reverse">
+                <div className="section-img">
+                    <img src="" alt="imagem" />
+                </div>
+
+                <div className="section-txt">
+                    <h2>Lorem ipsum dolor sit amet </h2>
+                    <p>Nemo, sequi sit? Qui ipsa rerum impedit explicabo modi, dolore fugiat quod deleniti provident perspiciatis vel at quia doloremque, nesciunt temporibus laudantium!</p>
+                </div>
+            </section>
+
+            <section className="section section3">
+                <div className="section-txt">
+                    <h2>Lorem ipsum dolor sit amet </h2>
+                    <p>Nemo, sequi sit? Qui ipsa rerum impedit explicabo modi, dolore fugiat quod deleniti provident perspiciatis vel at quia doloremque, nesciunt temporibus laudantium!</p>
+                </div>
+
+                <div className="section-img">
+                    <img src="" alt="imagem" />
+                </div>
+            </section>
+        </section>
+    )
+}
+
+export default About
