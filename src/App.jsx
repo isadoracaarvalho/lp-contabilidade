5150991e0bc1478076719cee06fe552eb4de242e
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-09-04 15:30:00 -0300

    organização apenas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Header from "./components/header"
+import Header from "./components/Header"
 import Hero from "./components/Hero"
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './styles/Header.css'
 import App from './App.jsx'
 
 import '@fontsource/inter/400.css';
