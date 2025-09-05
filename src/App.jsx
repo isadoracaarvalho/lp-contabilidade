bb58f8464b89caa40522cdda95061bdd57286a29
Isadora Carvalho <isadora.pereira@ucsal.edu.br>
2025-09-04 23:22:39 -0300

    form complete !!!

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header"
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Benefits from "./components/Benefits"
+import ContactForm from "./components/ContactForm"
 import Footer from "./components/Footer"
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Benefits />
+      <ContactForm />
       <Footer />
     </div>
   )
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,21 @@
+import '../styles/ContactForm.css'
+
+const ContactForm = () => {
+    return (
+        <section className="section-container">
+            <div className="title">
+                <h1>Entre em contato</h1>
+                <h2>Resposta rápida por email.</h2>
+            </div>
+
+            <form action="">
+                <input placeholder="Nome" type="text" required/>
+                <input placeholder="Email" type="email" required/>
+                <textarea placeholder="Mensagem" rows={8} required></textarea>
+                <input type="submit" value="ENVIAR" />
+            </form>
+        </section>
+    )
+}
+
+export default ContactForm
